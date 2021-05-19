cffbd96a04bd83e4ad20bf40c4630e549120a79b
blackieaka007 <78746663+blackieaka007@users.noreply.github.com>
2021-05-19 22:41:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
+# -*- encoding: utf-8 -*-
+
+import flask
 from flask import *
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def main():
-    return render_template("home.html") 
+    return render_template("main.html")
+
+
 
 
 if __name__ == "__main__":
